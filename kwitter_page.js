dc0142422b28e7608a5157e21a881c0f472d51c6
nikhil-babu0707 <87303388+nikhil-babu0707@users.noreply.github.com>
2021-12-04 18:07:53 +0530

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -10,13 +10,28 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 function getData() {
-    firebase.database().ref("/").on('value', function(snapshot) {
+    firebase.database00().ref("/" + room_name).on('value', function(snapshot) {
         document.getElementById("output").innerHTML = "";
         snapshot.forEach(function(childSnapshot) {
             childKey = childSnapshot.key;
-            Room_names = childKey;
+            childData = childSnapshot.val();
+            if (childKey != "purpose") {
+                firebase_message_id = childKey;
+                message_data = childData;
+            }
         });
     });
+}
+getData();
+
+function send() {
+    msg = document.getElementById("msg").value;
+    firebase.database().ref(room_name).push({
+        name: user_name,
+        message: msg,
+        like: 0
+    });
+    document.getElementById("msg").value = "";
 }
 
 function logout() {
@@ -24,4 +39,3 @@ function logout() {
     localStorage.removeItem("room_name");
     window.location = "index.html";
 }
-getData();
